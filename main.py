3e2664d00423097159646db975f4a1ba00053de3
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-09-21 12:26:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from settings import WIDTH, HEIGHT, FPS
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Zelda")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill("black")
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
