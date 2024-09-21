e6e707255e42ceffdbf606ae7c63c593ac3685d4
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-09-21 14:20:00 +0100

    feat: sort sprites by y position and allow hitbox overlap

--- tile.py
+++ tile.py
@@ -4,5 +4,6 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups) -> None:
         super().__init__(groups)
-        self.image = pygame.image.load("graphics/test/rock.png")
-        self.rect = self.image.get_rect(topleft=pos)
+        self.image = pygame.image.load("graphics/test/rock.png").convert_alpha()
+        self.rect: pygame.Rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
