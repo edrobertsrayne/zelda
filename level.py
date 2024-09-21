3c42550b9477cfb15cae873f78cf73e8e13fd3dd
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-09-21 14:06:07 +0100

    feat: simple camera

--- level.py
+++ level.py
@@ -1,5 +1,4 @@
 import pygame
-from debug import debug
 from tile import Tile
 from player import Player
 from settings import TILESIZE, WORLD_MAP
@@ -8,7 +7,7 @@ from settings import TILESIZE, WORLD_MAP
 class Level:
     def __init__(self) -> None:
         self.display_surface = pygame.display.get_surface()
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
         self.create_map()
 
@@ -25,6 +24,23 @@ class Level:
                     )
 
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self) -> None:
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
