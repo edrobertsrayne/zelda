5f766b04a21b844d2c79119964c4f90b0ae6f22f
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-09-22 12:45:47 +0100

    fix: remove debuging code

--- support.py
+++ support.py
@@ -19,7 +19,6 @@ def import_folder(path):
         img_files.sort()
         for image in img_files:
             full_path = path + "/" + image
-            print(full_path)
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
     return surface_list
