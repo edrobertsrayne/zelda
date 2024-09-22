571d053ffadd9e0268f371b89fd4b404cbec5279
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-09-22 12:44:14 +0100

    feat: add new world map

--- tile.py
+++ tile.py
@@ -1,9 +1,18 @@
 import pygame
+from settings import TILESIZE
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups) -> None:
+    def __init__(
+        self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))
+    ) -> None:
         super().__init__(groups)
-        self.image = pygame.image.load("graphics/test/rock.png").convert_alpha()
-        self.rect: pygame.Rect = self.image.get_rect(topleft=pos)
+        self.sprite_type = sprite_type
+        self.image = surface
+        if self.sprite_type == "object":
+            self.rect: pygame.Rect = self.image.get_rect(
+                topleft=(pos[0], pos[1] - TILESIZE)
+            )
+        else:
+            self.rect: pygame.Rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
